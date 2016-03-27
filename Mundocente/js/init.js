ab6b486d7983c8b74f2eab6cb038e1256dc86974
Diego Sosa <diego.sosa0704@gmail.com>
2016-03-27 14:31:35 -0500

    Agrego carpeta Mundocente con diseño inicial de la plataforma web

---
+++ Mundocente/js/init.js
@@ -0,0 +1,10 @@
+/**
+ * Created by SOSA-PC on 27/03/2016.
+ */
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
