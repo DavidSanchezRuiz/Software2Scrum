d8043cf34d81420b964f7dfd659e314cf9e78205
Diego Sosa <diego.sosa0704@gmail.com>
2016-04-01 21:29:27 -0500

    Agrego actualizacion del diseño web para la aplicacion

--- Mundocente/js/init.js
+++ Mundocente/js/init.js
@@ -8,3 +8,19 @@
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Paralax
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
+var options = [
+    {selector: '#Services', offset: 200, callback: 'Materialize.fadeInImage("#Services")' },
+    {selector: '#News', offset: 200, callback: 'Materialize.fadeInImage("#News")' },
+    {selector: '#Contact', offset: 200, callback: 'Materialize.fadeInImage("#Contact")' },
+];
+Materialize.scrollFire(options);
