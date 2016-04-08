dfc8b3272ad67d91718f6b7e81cf8b960a5ffe94
Diego Sosa <diego.sosa0704@gmail.com>
2016-04-08 09:11:58 -0500

    Agrego actualizacion de diseño en index.html y una nueva seccion llamada search.html

--- Mundocente/Front/js/init.js
+++ Mundocente/Front/js/init.js
@@ -11,13 +11,9 @@
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-});
-
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-});
-
-$(document).ready(function() {
     $('select').material_select();
+    $('.collapsible').collapsible({
+        accordion : false
+    });
 });
